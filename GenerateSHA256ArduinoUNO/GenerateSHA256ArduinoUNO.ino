9fe2dc5589133dcdeab8707092ce75d970ef8e21
Manuel Montenegro <manumonti@gmail.com>
2017-12-28 18:49:38 +0100

    Added HMAC & Diffie-Hellman test sketches

---
+++ GenerateSHA256ArduinoUNO/GenerateSHA256ArduinoUNO.ino
@@ -0,0 +1,66 @@
+/****************************************************************************/
+/*
+ * GenerateSHA256ArduinoUNO
+ * Created by Manuel Montenegro, December 27, 2017.
+ * Developed by Manuel Montenegro for Final Year Project. 
+ * 
+ *  This sketch generates a SHA256 Cryptographic hash from a string and 
+ *  print the elapsed time during processing.
+ *  
+ *  Compatible boards with this sketch: Arduino UNO and Intel Galileo.
+*/
+/****************************************************************************/
+
+#include <SHA256.h>
+
+// This object is used to manage SHA256 functionalities
+SHA256 sha256;
+
+// Data for calculating the HMAC
+const char data [] = "Lorem ipsum dolor sit amet";
+
+// Key used for HMAC
+const char key [] = "key";
+
+void setup() {
+  Serial.begin(9600);
+
+  // Flush previous data in the serial port
+  Serial.println();
+
+  // Array where is going to be placed the HMAC
+  uint8_t result [sha256.hashSize()];
+
+  // Time at the beginning of the ciphering. For calculating the elapsed time
+  unsigned long timeStart = micros();
+
+  // Calculating the HMAC...
+  sha256.resetHMAC(key, strlen(key));
+  sha256.update(data, strlen(data));
+  sha256.finalizeHMAC(key, strlen(key), result, sizeof(result));
+
+  // Time at the end of ciphering.
+  unsigned long timeEnd = micros();
+
+  // Printing the information...
+  Serial.print ("Key: ");
+  Serial.println (key);
+  Serial.print ("Data: ");
+  Serial.println (data);
+  Serial.println ();
+
+  // Printing the calculated HMAC
+  for (int i = 0; i < sizeof(result); i++) {
+    Serial.print (result[i], HEX);
+  }
+
+  //  Printing the elapsed time
+  Serial.println();
+  Serial.print("Elapsed time during ciphering (in microseconds): ");
+  Serial.println(timeEnd-timeStart);
+
+}
+
+void loop() {
+
+}
---
+++ SimulateDHArduinoUno/SimulateDHArduinoUno.ino
@@ -0,0 +1,129 @@
+/****************************************************************************/
+/*
+ * SimulateDHArduinoUno
+ * Created by Manuel Montenegro, December 27, 2017.
+ * Developed by Manuel Montenegro Final Year Project.
+ * 
+ *  This sketch simulates a Diffie-Hellman key exchange between two devices
+ *  and prints the elapsed time during processing.
+ *  
+ *  Compatible boards with this sketch: Arduino UNO and Intel Galileo.
+*/
+/****************************************************************************/
+
+#include <Curve25519.h>
+#include <RNG.h>
+
+// Print a generated key in hexadecimal
+void printKey ( uint8_t key [32] ) {
+  for (int i = 0; i < 32; i++) {
+    Serial.print (key [i], HEX);
+  }
+  Serial.println();
+}
+
+void setup() {
+
+  // Save a new seed for generating non-repeated random numbers
+  RNG.begin("SimulateDHArduinoUno", 950);
+  
+  Serial.begin (9600);
+
+  // Flush previous data in the serial port
+  Serial.println();
+
+  // Arrays for Diffie-Hellman keys
+  uint8_t alice_k [32];
+  uint8_t alice_f [32];
+  uint8_t bob_k [32];
+  uint8_t bob_f [32];
+
+  // For calculating elapsed time
+  unsigned long timeStart;
+  unsigned long timeEnd;
+  
+
+  // Alice k/f generation .....................................................
+  Serial.println("Generating random k/f for Alice ...");
+
+  // Time at the beginning of the process. For calculating the elapsed time
+  timeStart = micros();
+  
+  // Generate a 'f' (private) and a 'k' (public) value for Alice
+  Curve25519::dh1 (alice_k, alice_f);
+
+  // Time at the end of the process
+  timeEnd = micros();
+
+  Serial.print( "Alice k: " );
+  printKey (alice_k);
+  Serial.print( "Alice f: " );
+  printKey (alice_f);
+  Serial.print( "Elapsed time (in microseconds): " );
+  Serial.println( timeEnd - timeStart );
+  Serial.println();
+
+
+  // Bob  k/f generation ......................................................
+  Serial.println("Generating random k/f for Bob ...");
+
+  // Time at the beginning of the process. For calculating the elapsed time
+  timeStart = micros();
+
+  // Generate a 'f' (private) and a 'k' (public) value for Bob.
+  Curve25519::dh1 (bob_k, bob_f);
+
+  // Time at the end of the process
+  timeEnd = micros();
+
+  Serial.print( "Bob k: " );
+  printKey (bob_k);
+  Serial.print( "Bob f: " );
+  printKey (bob_f);
+  Serial.print( "Elapsed time (in microseconds): " );
+  Serial.println( timeEnd - timeStart );
+  Serial.println();
+
+
+  // Alice shared secret generation .........................................
+  Serial.println ("Generating shared secret for Alice ...");
+  
+  // Time at the beginning of the process. For calculating the elapsed time
+  timeStart = micros();
+
+  // Generate shared secret for Alice and destroy the private key
+  Curve25519::dh2(bob_k, alice_f);
+
+  // Time at the end of the process
+  timeEnd = micros();
+
+  Serial.print( "Alice shared key: " );
+  printKey (bob_k);
+
+  Serial.print( "Elapsed time (in microseconds): " );
+  Serial.println( timeEnd - timeStart );
+  Serial.println();
+
+
+  // Bob shared secret generation .........................................
+  Serial.println ("Generating shared secret for Bob ...");
+  
+  // Time at the beginning of the process. For calculating the elapsed time
+  timeStart = micros();
+
+  // Generate shared secret for Alice and destroy the private key
+  Curve25519::dh2(alice_k, bob_f);
+
+  // Time at the end of the process
+  timeEnd = micros();
+
+  Serial.print( "Bob shared key: " );
+  printKey (alice_k);
+
+  Serial.print( "Elapsed time (in microseconds): " );
+  Serial.println( timeEnd - timeStart );
+  Serial.println();
+}
+
+void loop() {
+}
