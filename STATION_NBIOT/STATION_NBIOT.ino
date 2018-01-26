787115668b2d0247d2b3d34431260e9078225fcd
Manuel Montenegro <manumonti@gmail.com>
2018-01-26 15:29:06 +0100

    Added sketches for SODAQ NB-IoT Shield

---
+++ STATION_NBIOT/STATION_NBIOT.ino
@@ -0,0 +1,17 @@
+/*********************************************************************************************/
+/*
+ * STATION_NBIOT
+ * Created by Manuel Montenegro, January 23, 2017.
+ * Developed for Manuel Montenegro Bachelor Thesis
+ * 
+ *  This sketch manages each station of the platform and use NB-IoT Sodaq Shield with ublox
+ *  N211 for communication by NB-IoT. This connection is used for publish temporary results of
+ *  a sport event.
+ * 
+ *  Serial port "Serial" is for USB debug. Serial port "Serial1" stablish a serial 
+ *  communication with NB-IoT modem ublox N211.
+ *  
+ *  Compatible boards with this sketch: Arduino Leonardo.
+*/
+/*********************************************************************************************/
+
