68f548aac3d1251354c1f21e8d866c1228313432
Manuel Montenegro <manumonti@gmail.com>
2018-01-27 21:43:05 +0100

    Modified all the library from beginning.
    The library catchs all the serial communication between PC and Arduino

--- libraries/SerialInterface/SerialInterface.cpp
+++ libraries/SerialInterface/SerialInterface.cpp
@@ -20,3 +20,133 @@ SerialInterface::SerialInterface () {
 }
 
 
+// Master device sends the ID of next station and receives what want to do the user
+uint8_t SerialInterface::sendStationIdReceiveChoice (uint8_t staID) {
+
+	uint8_t choice;
+
+	Serial.println(staID);				// Sends the station ID to PC
+
+	while (!Serial.available());		// Waits until serial data is detected
+	delay(10);							// Waits serial buffer receives all data
+	choice = Serial.read();				// Saves user choice
+	while (Serial.available()) {		// Cleans the serial buffer (return carriage)
+		Serial.read();
+	}
+
+	return choice;
+
+}
+
+
+// Master device receives what want to do the user
+uint8_t SerialInterface::receiveChoice () {
+
+	uint8_t choice;
+
+	while (!Serial.available());		// Waits until serial data is detected
+	delay(10);							// Waits serial buffer receives all data
+	choice = Serial.read();				// Saves user choice
+	while (Serial.available()) {		// Cleans the serial buffer (return carriage)
+		Serial.read();
+	}
+
+	return choice;
+}
+
+
+uint8_t SerialInterface::receiveChoiceSendAck () {
+
+	uint8_t choice;
+
+	while (!Serial.available());		// Waits until serial data is detected
+	delay(10);							// Waits serial buffer receives all data
+	choice = Serial.read();				// Saves user choice
+	while (Serial.available()) {		// Cleans the serial buffer (i.e. return carriage)
+		Serial.read();
+	}
+ 
+	Serial.println('1');				// Send ACK
+
+	return choice;
+}
+
+
+// Master device receives user name by USB serial port
+void SerialInterface::receiveName ( uint8_t *name ) {
+
+	uint8_t count;						// A simple counter
+
+	while (!Serial.available());		// Waits until serial data is detected
+	delay(10);							// Waits serial buffer receives all data
+	count = Serial.readBytes(name, NAME_SIZE-1);	// Read user name and saves it
+	name[count]='\0';					// Put null terminated
+	while (Serial.available()) {		// Clean the serial buffer
+		Serial.read();
+	}
+
+}
+
+
+// Master device receives user name by USB serial port
+void SerialInterface::receiveCategory ( uint8_t *category) {
+
+	uint8_t count;						// A simple counter
+
+	while (!Serial.available());		// Waits until serial data is detected
+	delay(10);							// Waits serial buffer receives all data
+	count = Serial.readBytes (category, CAT_SIZE-1);// Read user category and saves it
+	category[count]='\0';				// Put null terminated
+	while (Serial.available()) {		// Clean the serial buffer
+		Serial.read();
+	}
+
+}
+
+
+// Master device recives date by Serial in format "mmm dd yyyy"
+void SerialInterface::receiveDate ( uint8_t *dateReceived ) {
+
+	while (!Serial.available());		// Waits until serial data is detected
+	delay(10);							// Waits serial buffer receives all data
+	Serial.readBytes(dateReceived, STRING_DATE_SIZE);// Read time and saves it
+
+}
+
+
+// Master device recives time by Serial in format "hh:mm:ss"
+void SerialInterface::receiveTime ( uint8_t *timeReceived ) {
+
+	while (!Serial.available());		// Waits until serial data is detected
+	delay(10);							// Waits serial buffer receives all data
+	Serial.readBytes(timeReceived, STRING_TIME_SIZE);// Read time and saves it
+
+}
+
+
+// Master device send a byte with any value by USB
+void SerialInterface::sendChar (uint8_t character) {
+	Serial.println(character);
+}
+
+
+// Master device sends a String by USB serial
+void SerialInterface::sendString (uint8_t *string) {
+	Serial.println((char*)string);
+}
+
+
+
+// Master device sends a hexadecimal array in the correct way
+void SerialInterface::sendHexString(uint8_t array[], uint8_t len) {
+	char buffer [(len*2)+1];
+	for (uint8_t i = 0; i < len; i++) {
+		byte nib1 = (array[i] >> 4) & 0x0F;
+		byte nib2 = (array[i] >> 0) & 0x0F;
+		buffer[i*2+0] = nib1  < 0xA ? '0' + nib1  : 'A' + nib1  - 0xA;
+		buffer[i*2+1] = nib2  < 0xA ? '0' + nib2  : 'A' + nib2  - 0xA;
+	}
+	buffer[len*2] = '\0';
+
+	Serial.println(buffer);
+}
--- libraries/SerialInterface/SerialInterface.h
+++ libraries/SerialInterface/SerialInterface.h
@@ -20,15 +20,30 @@
 	#include "WProgram.h"
 #endif
 
+
+#define CAT_SIZE			15			// Size in bytes of category field in card
+#define NAME_SIZE			16			// Size in bytes of player name field in card
+#define STRING_TIME_SIZE	8			// Size in bytes of time in format hh:mm:ss
+#define STRING_DATE_SIZE	11			// Size in bytes of date in format mmm dd yyyy
+
+
 class SerialInterface {
 public:
-	SerialInterface ();
-
+	SerialInterface ();					// Class constructor
+	uint8_t sendStationIdReceiveChoice (uint8_t staID);	// Master sends to PC station ID
+	uint8_t receiveChoice ();			// Master receives a byte with user's choice
+	uint8_t receiveChoiceSendAck();		// Master receives user's choice and sends ACK
+	void receiveName ( uint8_t *name );	// Master receives a user name for card by USB
+	void receiveCategory ( uint8_t *category );	// Master receives a user name for card by USB
+	void receiveTime ( uint8_t *timeReceived );	// Master receives time 
+	void receiveDate ( uint8_t *dateReceived );	// Master receives date
+	void sendChar (uint8_t character);	// Master sends a byte to PC
+	void sendString (uint8_t *string);	// Master sends a string to PC
+	void sendHexString(uint8_t array[], uint8_t len); // Master sends a hex string to PC
 
 private:
 
 
-	
 };
 
 #endif
