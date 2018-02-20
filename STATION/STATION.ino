3ecbaf6b1aaa6d02799c4659c1cf9881c8dd0a8a
Manuel Montenegro <manumonti@gmail.com>
2018-02-20 16:54:29 +0100

    Deleted redundant data

--- STATION/STATION.ino
+++ STATION/STATION.ino
@@ -1,7 +1,7 @@
 /*********************************************************************************************/
 /*
  * STATION
- * Created by Manuel Montenegro, January 28, 2017.
+ * Created by Manuel Montenegro, January 28, 2018.
  * Developed for Manuel Montenegro Bachelor Thesis. 
  * 
  *  This sketch manages each station of the platform.
@@ -15,27 +15,23 @@
 #include <SetUpStations.h>          // Stations' setup library
 #include <PlayerCard.h>             // User's card management library
 
-#include <RTClib.h>
-
 #define LED_PIN           9         // Digital Pin where is tied LED
 #define CARD_TIMEOUT      2         // Number of seconds between punch
 
 PlayerCard card;                    // Manages operation with user cards
 
-
-uint8_t cardBlock = 4;
-
-
 void setup() {
   
   Serial.begin (115200);            // Sets up serial port baudrate (ONLY FOR DEBUG)  
-  while (!Serial);
+
+  while (!Serial);                  // Only for debug
   
   pinMode(LED_PIN, OUTPUT);         // Set Up digital pin for LED
   digitalWrite(LED_PIN, HIGH);      // Turn on LED for indicating set up period has started
 
-//  StationNewSetUp stationSetUp;     // Manages the stations' setup
-//  stationSetUp.startNewSetUp ();    // Starts the process of setting up station
+  StationNewSetUp stationSetUp;     // Manages the stations' setup
+
+  stationSetUp.startNewSetUp ();    // Starts the process of setting up station
 
   card.begin();
 
