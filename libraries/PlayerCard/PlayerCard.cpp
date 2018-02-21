dc5b84660dd047bca6daf81e40ad947bd7fbf38f
Manuel Montenegro <manumonti@gmail.com>
2018-02-21 17:15:56 +0100

    Finished read  and validate punches method

--- libraries/PlayerCard/PlayerCard.cpp
+++ libraries/PlayerCard/PlayerCard.cpp
@@ -1,7 +1,7 @@
 /*********************************************************************************************/
 /*
  * Players' NFC card management Arduino library
- * Created by Manuel Montenegro, January 28, 2017.
+ * Created by Manuel Montenegro, February 20, 2018.
  * Developed for Manuel Montenegro Bachelor Thesis. 
  * 
  *  This library is used for the management of players' cards. Allows operation like reading
@@ -31,6 +31,7 @@ PlayerCard::PlayerCard () : nfc( PN532_IRQ, PN532_RESET ) { }
 void PlayerCard::begin() {
 	nfc.begin();						// I2C initialization & resets PN532 module
 	nfc.SAMConfig();					// Configures the Secure Access Module of PN532
+	i2cEeprom=AT24C32(I2C_EEPROM_ADDR);	// Inits I2C EEPROM in RTC module in I2C address
 	rtc.begin();						// Inits Real Time Clock hardware
 
 	EEPROM.get (ID_STATION_ADDR, idStation);	// Loads the assigned ID of this station
@@ -78,17 +79,23 @@ void PlayerCard::format () {
 void PlayerCard::readPunches () {
 
 	uint8_t uid[7];						// UID of user's card
-	uint8_t uidLength;					// Length of the UID (depends on card type)
-	uint8_t cardBlock;					// For storing next card's block for writting
 	uint8_t data[MIFARE_BLOCK_SIZE];	// For storing block data during reads
-	uint8_t previousBlockData [MIFARE_BLOCK_SIZE];
-	uint8_t success;					// Control flag
+	uint8_t dataPrevBlock[MIFARE_BLOCK_SIZE];
 
-	uint8_t lastBlock;					// Next free block in card
+	uint8_t lastBlock;					// Next free block in card & finish block
 	uint8_t category [CAT_SIZE];		// Char array with player category
 	uint8_t name [NAME_SIZE];			// Char array with player name
+	uint8_t ids;						// Station identifier
+	uint32_t punchTime;					// Time of current punch
+	uint8_t punchTimeParsed [3];		// Time of current punch parsed
+	uint8_t authCode[AUTH_IN_CARD_SIZE];// Authentication code
+	uint8_t genMac[AUTH_IN_CARD_SIZE];	// Generated MAC for compare with auth code received
 
 	uint8_t blockPointer;				// Pointer to the next card block to be readed
+	uint8_t value;						// Auxiliar value for using memcpy function
+	uint8_t punchAuthenticated;			// This will be true if the puch is authenticated
+
+
 
 	readCardHeader(uid, &lastBlock, category, name);// Reads info from card header
 
@@ -99,24 +106,69 @@ void PlayerCard::readPunches () {
 
 	blockPointer = FIRST_PUNCH_BLOCK;	// blockPointer starts pointing to first punch block
 
+
 	while (blockPointer < lastBlock) {	// Reads each punch from card
 
-		// Check if this is a new block so that we can reauthenticate
-		if (nfc.mifareclassic_IsFirstBlock(blockPointer)) {
-
-			// Authenticates sector
+		// The first punch is a special case, because the authentication is with block 1
+		if (blockPointer == FIRST_PUNCH_BLOCK) {
+			// BLOCK 1 was authenticated in readCardHeader method
+			nfc.mifareclassic_ReadDataBlock(previousBlock(blockPointer), dataPrevBlock);
+			// This is necessary because NB# change. For authentication must be 0
+			dataPrevBlock[0] = 0;
 			nfc.mifareclassic_AuthenticateBlock (uid, 4, blockPointer, keyBType, keyb);
+			nfc.mifareclassic_ReadDataBlock(blockPointer, data);
+		
+		} else {						// If this isn't the first punch block
 
-			Serial.println("Sector autenticado!");
+			// Check if this is a new block so that we can reauthenticate
+			if (nfc.mifareclassic_IsFirstBlock(blockPointer)) {
+				// Authenticates sector
+				nfc.mifareclassic_AuthenticateBlock (uid, 4, blockPointer, keyBType, keyb);
+			}
+
+			// Read the current memory block
+			nfc.mifareclassic_ReadDataBlock(blockPointer, data);
 
 		}
 
-		Serial.print("Registro: ");
-		Serial.println(blockPointer);
+		// Parse the data readed from the block
+		ids = data [0];					
+		memcpy(&punchTime, &data[1], sizeof(punchTime));
+		memcpy(authCode, &data[5], AUTH_IN_CARD_SIZE);
+		
+		// Convert time in char array
+		DateTime dateTime (punchTime);
+		value = dateTime.hour();
+		memcpy (&punchTimeParsed[0], &value, 1);
+		value = dateTime.minute();
+		memcpy (&punchTimeParsed[1], &value, 1);
+		value = dateTime.second();
+		memcpy (&punchTimeParsed[2], &value, 1);
 
+		// Validates the punch
+		// Reads the key of the stations that generated the punch
+		loadStationKey (ids);
+		generateMac (genMac, uid, ids, punchTime, dataPrevBlock);
+
+
+		if (memcmp (genMac, authCode, sizeof(authCode)) == 0) {
+			punchAuthenticated = true;
+		} else {
+			punchAuthenticated = false;
+		}
+		
+
+		// Send the data by serial port
+		usb.sendContinue (true);
+		usb.sendPunchData (data[0], punchTimeParsed, punchAuthenticated);
+
+		// Save the current block in previous block array
+		memcpy (dataPrevBlock, data, sizeof(data));
 		blockPointer = nextFreeBlock(blockPointer);	// Updates the value of blockPointer
 
 	}
+
+	usb.sendContinue (false);
 
   }
 
@@ -157,6 +209,12 @@ void PlayerCard::punch ( ) {
 		
 				// Reads the data of the block and parse it
 				if ( nfc.mifareclassic_ReadDataBlock(previousBlock (cardBlock), previousBlockData) ) {
+
+					/* This is necessary because NB# change along punches. So this must be
+						constant for doing authentication */
+					if (cardBlock == FIRST_PUNCH_BLOCK) {
+						previousBlockData[0] = 0;
+					}
 
 					buildPunchRecord(cardBlock, previousBlockData, data, uid);	// Takes the punch record information
 
@@ -208,15 +266,48 @@ void PlayerCard::buildPunchRecord ( uint8_t currentBlock, uint8_t *lastBlockData
 
 /* Generates a Message Authentication code for a punch record
 isFirstRecord must be 1 if is the first record or 0 if not*/
-void PlayerCard::generateMac (uint8_t *mac, uint8_t uid, uint8_t ids, uint32_t time, uint8_t *lastBlockData ) {
+void PlayerCard::generateMac (uint8_t *mac, uint8_t *uid, uint8_t ids, uint32_t time, uint8_t *lastBlockData ) {
+
+			// Serial.print("stationKey: ");
+			// for (int i = 0; i < STATION_REC_SIZE; i++) {
+			// 	Serial.print (stationKey[i],HEX);
+			// 	Serial.print (" ");
+			// }
+			// Serial.println();
+			// Serial.print("uid: ");
+			// for (int i = 0; i < 4; i++) {
+			// 	Serial.print (uid[i],HEX);
+			// 	Serial.print (" ");
+			// }
+			// Serial.println();
+			// Serial.println();
+			// Serial.print("ids: ");
+			// Serial.print(ids);
+			// Serial.println();
+			// Serial.print("time: ");
+			// Serial.print(time);
+			// Serial.println();
+			// Serial.print("Last block data: ");
+			// for (int i = 0; i < 16; i++) {
+			// 	Serial.print (lastBlockData[i],HEX);
+			// 	Serial.print (" ");
+			// }
+			// Serial.println();
 
 	// Blake2s for authenticating the punch record
 	blake.reset(stationKey, sizeof(stationKey), AUTH_IN_CARD_SIZE);
-	blake.update(&uid, sizeof(uid));
+	blake.update(uid, 4);
 	blake.update(&ids, sizeof(ids));
 	blake.update(&time, sizeof(time));
 	blake.update(lastBlockData, MIFARE_BLOCK_SIZE);
 	blake.finalize(mac, AUTH_IN_CARD_SIZE);
+
+			// Serial.print("MAC generated: ");
+			// for (int i = 0; i < AUTH_IN_CARD_SIZE; i++) {
+			// 	Serial.print (mac[i],HEX);
+			// 	Serial.print (" ");
+			// }
+			// Serial.println();
 
 }
 
@@ -338,6 +429,13 @@ void PlayerCard::writeCardHeader (uint8_t nb, uint8_t *cat, uint8_t *name) {
 
 		}
 	}
+
+}
+
+
+void PlayerCard::loadStationKey (uint8_t ids) {
+	uint8_t position = (ids*STATION_REC_SIZE);
+	i2cEeprom.read(position, stationKey, STATION_REC_SIZE);
 
 }
 
--- libraries/PlayerCard/PlayerCard.h
+++ libraries/PlayerCard/PlayerCard.h
@@ -1,7 +1,7 @@
 /*********************************************************************************************/
 /*
  * Players' NFC card management Arduino library
- * Created by Manuel Montenegro, January 28, 2017.
+ * Created by Manuel Montenegro, February 20, 2018.
  * Developed for Manuel Montenegro Bachelor Thesis. 
  * 
  *  This library is used for the management of players' cards. Allows operation like reading
@@ -36,6 +36,7 @@
 #include <RTClib.h>						// Real Time Clock library
 #include <EEPROM.h>						// Arduino EEPROM management library
 #include <SerialInterface.h>			// Serial communication with PC library
+#include <AT24CX.h>						// I2C EEPROM in RTC module management library
 
 
 #ifdef ARDUINO_AVR_UNO
@@ -52,6 +53,7 @@
 #define TIME_SIZE			4			// Size in bytes of clock time
 #define AUTH_IN_CARD_SIZE	11			// Size of HMAC in each punch record in user's card	
 #define MIFARE_BLOCK_SIZE	16			// Size of each block on Mifare Classic 1k Card
+#define STATION_REC_SIZE	32			// Size in bytes of each station record
 #define UID_LENGTH			4			// Length of UID in Mifare Classic 1k cards
 #define NB_CAT_BLOCK		1			// Block number of Next Block and Category in user card
 #define NAME_BLOCK			2			// Block number of User Name in user card
@@ -59,6 +61,7 @@
 #define LAST_PUNCH_BLOCK	62			// Block number of last block that can be written
 #define ID_STATION_ADDR		0			// Arduino EEPROM address where is stored station ID
 #define KEY_EEPROM_ADDR		50			// Arduino EEPROM address where is stored station Key
+#define I2C_EEPROM_ADDR		0x57		// I2C Address of EEPROM integrated in RTC module
 
 
 class PlayerCard {
@@ -76,6 +79,7 @@ private:
 	BLAKE2s blake;						// Object that manages Blake2s crypto functionalities
 	RTC_DS3231 rtc;						// Object that manages Real Time Clock
 	SerialInterface usb;				// Serial Interface for communicating by USB port
+	AT24CX i2cEeprom;					// Manages I2C EEPROM in RTC module
 
 	// Key B for authenticates sectors of Mifare Classic Card
 	const uint8_t keyb [6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
@@ -87,9 +91,10 @@ private:
 	void writeCardHeader (uint8_t nb, uint8_t *cat, uint8_t *name);	// Writes card header info
 	void buildPunchRecord ( uint8_t currentBlock, uint8_t *lastBlockData, uint8_t *block, uint8_t *uid );	// Builds the next record
 	// Method that generates a message authentication code with blake2s
-	void generateMac (uint8_t *mac, uint8_t uid, uint8_t ids, uint32_t time, uint8_t *lastBlockData );
+	void generateMac (uint8_t *mac, uint8_t *uid, uint8_t ids, uint32_t time, uint8_t *lastBlockData );
 	uint8_t nextFreeBlock ( uint8_t cardBlock );// Return the following free block of card
 	uint8_t previousBlock ( uint8_t cardBlock );// Return the last written block
+	void loadStationKey (uint8_t ids);	// Master searchs in EEPROM the key for this IDS
 
 };
 
