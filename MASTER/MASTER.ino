a031507b02bd3d5374612d85982db148f2e1cee5
Manuel Montenegro <manumonti@gmail.com>
2018-03-02 12:05:58 +0100

    MASTER sketch for Arduino MEGA (no memory requeriments)

---
+++ MASTER/MASTER.ino
@@ -0,0 +1,53 @@
+
+/*********************************************************************************************/ 
+/* 
+ * MASTER 
+ * Created by Manuel Montenegro, February 26, 2017. 
+ * Developed for Manuel Montenegro Bachelor Thesis.  
+ *  
+ *  This sketch sets up station devices by NFC. It assignes an identifier (ID) and a shared  
+ *  key using Diffie-Hellman algorithm. Persistent data is saved in EEPROM. 
+ *  
+ *  This sketch must be uploaded in master device. Serial connection to PC and NFC shield is  
+ *  required. Serial port baudrate: 115200 
+ *   
+ *  Compatible boards with this sketch: Arduino UNO, Arduino Leonardo & Arduino MEGA.
+*/ 
+/*********************************************************************************************/ 
+ 
+#include <SetUpStations.h>          // Stations' setup library 
+#include <PlayerCard.h>             // User's card management library 
+#include <SerialInterface.h>        // Serial communication with PC library 
+ 
+uint8_t userChoice;                 // User choose from serial menu 
+ 
+SerialInterface usb; 
+ 
+void setup() { 
+ 
+  Serial.begin (115200);            // Sets up serial port baudrate 
+  while (!Serial);                  // Waits until serial port is opened in PC 
+} 
+ 
+void loop() { 
+ 
+  userChoice = usb.receiveChoiceSendAck(); 
+   
+  if (userChoice == '1') { 
+    MasterSetUpStations setUp;      // Manages the stations' setup 
+    setUp.startNewEvent ();         // Starts the process of setting up new stations 
+  } else if (userChoice == '2') { 
+    MasterSetUpStations setUp;      // Manages the stations' setup 
+    setUp.continuePreviousEvent (); // Continues a previous process of setting up 
+  } else if (userChoice == '3') { 
+    PlayerCard card; 
+    card.begin();                   
+    card.format();                  // Formats a card
+  } else if (userChoice == '4') { 
+    PlayerCard card; 
+    card.begin(); 
+    card.readPunches();             // Reads and validates punches of a card
+  } 
+ 
+ 
+} 
